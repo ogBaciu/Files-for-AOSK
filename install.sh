bfe281bdded0b062fe881d2de82c1e97b15b6964
ogBaciu <31196737+ogBaciu@users.noreply.github.com>
2019-05-10 07:17:01 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+#script works with Raspbian and Kali Linux
 
-
-sudo apt-get install wireshark
-git clone https://github.com/ogBaciu/Files-for-AOSK.git
-
-sudo useradd -m -p snoopy gerry
-cp /Files/meeting.txt /etc/default/gerry
+if [ $OSTYPE == "linux-gnueabihf" ]
+# if OS is Raspbian
+then
+  echo Y | sudo apt-get install wireshark
+  echo Y | sudo apt-get install john
+  echo Y | sudo adduser gerry
+  echo snoopy | passwd gerry
+  sudo cp /home/pi/Files-for-AOSK/Files/meeting.txt /home/gerry
+else
+#if OS is Kali
+  echo Y | sudo adduser gerry
+  echo snoopy | passwd gerry
+  sudo cp /home/pi/Files-for-AOSK/Files/meeting.txt /home/gerry
+fi
